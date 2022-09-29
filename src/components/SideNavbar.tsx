a07cd50711b1ee32f807e3c719b7f7ffcea3b1c2
milesrykerodazie <milesodazie@gamil.com>
2022-09-29 19:55:42 +0100

    sidebar update

--- src/components/SideNavbar.tsx
+++ src/components/SideNavbar.tsx
@@ -21,25 +21,25 @@ const SideNavbar = ({ open, setOpen }: stateTyp) => {
       <div
          className={
             open
-               ? 'lg:hidden fixed left-0 top-0 w-full h-screen bg-blue-500/20 z-80 ease-in-out duration-700'
-               : 'lg:hidden fixed -left-[100%] top-0 w-full h-screen bg-sm8/60 z-80 ease-in-out duration-900'
+               ? 'lg:hidden fixed left-0 top-0 w-full h-screen bg-gradient-to-r  from-black/60 to-fuchsia-700/40 dark:from-fuchsia-700/80 dark:to-black/10 z-80 ease-in-out duration-700'
+               : 'lg:hidden fixed -left-[100%] top-0 w-full h-screen bg-gradient-to-r  from-black/70 to-fuchsia-500/30 dark:from-fuchsia-600/60 dark:to-black/10 z-80 ease-in-out duration-900'
          }
          onClick={handleSidebarToggle}
       >
          <div
             className={
                open
-                  ? 'fixed top-0 left-0 h-screen bg-gradient-to-r from-blue-300 via-blue-200 to-blue-50 shadow-xl shadow-blue-500 w-[70%]  md:w-[50%] ease-in duration-900 z-80 pt-3 px-3'
-                  : 'fixed -left-[100%] top-0 ease-in duration-700 bg-gradient-to-r from-blue-300 via-blue-200 to-blue-50 shadow-xl shadow-blue-500 w-[75%] md:w-[55%] h-screen z-80 pt-3 px-3'
+                  ? 'fixed top-0 left-0 h-screen  bg-black  shadow-xl shadow-fuchsia-800 dark:bg-fuchsia-700 dark:shadow-black w-[70%]  md:w-[50%] ease-in duration-900 z-80 pt-3 px-3'
+                  : 'fixed -left-[100%] top-0 ease-in duration-700 bg-black/80 dark:bg-fuchsia-700/70 shadow-xl shadow-fuchsia-800 dark:shadow-black/70 w-[75%] md:w-[55%] h-screen z-80 pt-3 px-3'
             }
             onClick={(e) => e.stopPropagation()}
          >
             <div className='flex justify-end'>
                <div
                   onClick={handleSidebarToggle}
-                  className='flex items-center justify-center rounded-full w-7 h-7 bg-white'
+                  className='flex items-center justify-center rounded-full w-7 h-7 bg-fuchsia-700 dark:bg-black/90 '
                >
-                  <MdClose className='w-6 h-6 text-blue-500' />
+                  <MdClose className='w-6 h-6 dark:text-fuchsia-500 text-black/95' />
                </div>
             </div>
          </div>
