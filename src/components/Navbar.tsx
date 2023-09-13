028fe8c7f5a40a765e786862307fcdc673516044
Miles Ryker Odazie <milesodazie@gmail.com>
2023-09-13 12:31:30 +0100

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,107 +1,103 @@
-import { useEffect, useRef } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { useLogoutMutation } from '../redux/features/authApiSlice';
+import { useEffect, useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useLogoutMutation } from "../redux/features/authApiSlice";
 
-const customId = 'custom-id-yes';
+const customId = "custom-id-yes";
 
 const Navbar = ({ user }: any) => {
-   const [logout, { data: logoutData, isSuccess }] = useLogoutMutation();
+  const [logout, { data: logoutData, isSuccess }] = useLogoutMutation();
 
-   //reference for useEffect cleanup
-   const effectRan = useRef(false);
+  //reference for useEffect cleanup
+  const effectRan = useRef(false);
 
-   const navigate = useNavigate();
+  const navigate = useNavigate();
 
-   //for authomatically getting post owner on login
-   let userDetail: any;
-   const userName = localStorage.getItem('user');
-   if (userName) {
-      userDetail = JSON.parse(userName);
-   }
+  //for authomatically getting post owner on login
+  let userDetail: any;
+  const userName = localStorage.getItem("user");
+  if (userName) {
+    userDetail = JSON.parse(userName);
+  }
 
-   // @ts-expect-error
-   useEffect(() => {
-      if (
-         effectRan.current === true ||
-         process.env.NODE_ENV !== 'development'
-      ) {
-         if (isSuccess) {
-            toast.success(logoutData?.message, {
-               toastId: customId,
-            });
-            localStorage.removeItem('user');
-            navigate('/');
-         }
+  // @ts-expect-error
+  useEffect(() => {
+    if (effectRan.current === true || process.env.NODE_ENV !== "development") {
+      if (isSuccess) {
+        toast.success(logoutData?.message, {
+          toastId: customId,
+        });
+        localStorage.removeItem("user");
+        navigate("/");
       }
-      return () => (effectRan.current = true);
-      // eslint-disable-next-line
-   }, [isSuccess, logoutData?.message]);
+    }
+    return () => (effectRan.current = true);
+    // eslint-disable-next-line
+  }, [isSuccess, logoutData?.message]);
 
-   const canCreatePost = userDetail !== undefined && user?.verified;
+  const canCreatePost =
+    userDetail !== undefined && user?.verified && userDetail?.role === "Admin";
 
-   return (
-      <nav className='space-x-2 flex items-center'>
-         {userDetail?.role === 'Admin' && (
-            <>
-               <Link to='/admin/userslist'>
-                  <p className='font-medium text-gray-900 dark:text-white duration-500 ease-in'>
-                     Users List
-                  </p>
-               </Link>
-               <p className='text-fuchsia-500 hidden lg:block'>|</p>
-            </>
-         )}
-         {canCreatePost && (
-            <>
-               <Link to='/create-post'>
-                  <p className='font-medium text-gray-900 dark:text-white duration-500 ease-in'>
-                     Create Post
-                  </p>
-               </Link>
-               <p className='text-fuchsia-500'>|</p>
-            </>
-         )}
-         <Link to='/about'>
-            <p className='font-medium text-gray-900 dark:text-white duration-500 ease-in'>
-               About Us
+  return (
+    <nav className="space-x-2 flex items-center">
+      {userDetail?.role === "Admin" && (
+        <>
+          <Link to="/admin/userslist">
+            <p className="font-medium text-gray-900 dark:text-white duration-500 ease-in">
+              Users List
             </p>
-         </Link>
-         <p className='text-fuchsia-500'>|</p>
-         <Link to='/contact'>
-            <p className='font-medium dark:text-white text-gray-900'>
-               Contact Us
+          </Link>
+          <p className="text-fuchsia-500 hidden lg:block">|</p>
+        </>
+      )}
+      {canCreatePost && (
+        <>
+          <Link to="/create-post">
+            <p className="font-medium text-gray-900 dark:text-white duration-500 ease-in">
+              Create Post
             </p>
-         </Link>
-         {userDetail === undefined && (
-            <>
-               <p className='text-fuchsia-500'>|</p>
-               <Link to='/auth'>
-                  <p className='font-medium dark:text-white text-gray-900'>
-                     Register
-                  </p>
-               </Link>
-               <p className='text-fuchsia-500'>|</p>
-               <Link to='/auth/login'>
-                  <button className='font-medium dark:text-white text-gray-900'>
-                     Login
-                  </button>
-               </Link>
-            </>
-         )}
-         {userDetail !== undefined && (
-            <nav className='space-x-2 flex items-center'>
-               <p className='text-fuchsia-500'>|</p>
-               <button
-                  className='dark:text-white text-gray-900 font-bold hover:text-fuchsia-600 duration-300 ease-in'
-                  onClick={logout}
-               >
-                  Logout
-               </button>
-            </nav>
-         )}
-      </nav>
-   );
+          </Link>
+          <p className="text-fuchsia-500">|</p>
+        </>
+      )}
+      <Link to="/about">
+        <p className="font-medium text-gray-900 dark:text-white duration-500 ease-in">
+          About Us
+        </p>
+      </Link>
+      <p className="text-fuchsia-500">|</p>
+      <Link to="/contact">
+        <p className="font-medium dark:text-white text-gray-900">Contact Us</p>
+      </Link>
+      {userDetail === undefined && (
+        <>
+          <p className="text-fuchsia-500">|</p>
+          <Link to="/auth">
+            <p className="font-medium dark:text-white text-gray-900">
+              Register
+            </p>
+          </Link>
+          <p className="text-fuchsia-500">|</p>
+          <Link to="/auth/login">
+            <button className="font-medium dark:text-white text-gray-900">
+              Login
+            </button>
+          </Link>
+        </>
+      )}
+      {userDetail !== undefined && (
+        <nav className="space-x-2 flex items-center">
+          <p className="text-fuchsia-500">|</p>
+          <button
+            className="dark:text-white text-gray-900 font-bold hover:text-fuchsia-600 duration-300 ease-in"
+            onClick={logout}
+          >
+            Logout
+          </button>
+        </nav>
+      )}
+    </nav>
+  );
 };
 
 export default Navbar;
